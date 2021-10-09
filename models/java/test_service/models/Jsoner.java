a135a612fe807e92fdf235606b9d7c4b960c254a
specgen-automation <automation@specgen.io>
2021-10-09 17:51:38 +0000

    [skip ci] Generated code with specgen 2.0.723

--- models/java/test_service/models/Jsoner.java
+++ models/java/test_service/models/Jsoner.java
@@ -10,15 +10,14 @@ public class Jsoner {
 	public static <T> String serialize(ObjectMapper objectMapper, T data) throws IOException {
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		objectMapper.registerModule(new JavaTimeModule())
-			.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-			.writeValue(byteArrayOutputStream, data);
-
+			.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		
+		objectMapper.writeValue(byteArrayOutputStream, data);
 		return byteArrayOutputStream.toString();
 	}
 
 	public static <T> T deserialize(ObjectMapper objectMapper, String jsonStr, Class<T> tClass) throws IOException {
 		InputStream inputStream = new ByteArrayInputStream(jsonStr.getBytes());
-
 		return objectMapper.readValue(inputStream, tClass);
 	}
 }
