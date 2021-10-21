1aae103186c1cdf5750cd1f023a90ca4205e99ff
specgen-automation <automation@specgen.io>
2021-10-21 11:16:56 +0000

    [skip ci] Generated code with specgen 2.0.788

--- services/java-spring/test_service/v2/controllers/EchoController.java
+++ services/java-spring/test_service/v2/controllers/EchoController.java
@@ -1,14 +1,16 @@
 package test_service.v2.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
 import java.io.IOException;
 import java.time.*;
 import java.util.UUID;
+
+import org.apache.logging.log4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.apache.tomcat.util.http.fileupload.FileUploadBase.CONTENT_TYPE;
 
@@ -18,6 +20,7 @@ import test_service.v2.services.echo.*;
 
 @RestController("EchoControllerV2")
 public class EchoController {
+	private static final Logger LOG = LogManager.getLogger(EchoController.class);
 
 	@Autowired
 	private EchoService echoService;
@@ -25,15 +28,23 @@ public class EchoController {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@PostMapping("v2/echo/body")
+	@PostMapping("/v2/echo/body")
 	public ResponseEntity<String> echoBodyController(@RequestBody String jsonStr) throws IOException {
-		var requestBody = objectMapper.readValue(jsonStr, Message.class);
-		var result = echoService.echoBody(requestBody);
-
+		LOG.info("Received request, operationId: echo.echo_body, method: POST, url: /v2/echo/body");
 		HttpHeaders headers = new HttpHeaders();
 		headers.add(CONTENT_TYPE, "application/json");
+
+		Message requestBody;
+		try {
+			requestBody = objectMapper.readValue(jsonStr, Message.class);
+		} catch (Exception e) {
+			LOG.error("Completed request with status code: {}", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(headers, HttpStatus.BAD_REQUEST);
+		}
+		var result = echoService.echoBody(requestBody);
 		String responseJson = objectMapper.writeValueAsString(result);
 
+		LOG.info("Completed request with status code: {}", HttpStatus.OK);
 		return new ResponseEntity<>(responseJson, headers, HttpStatus.OK);
 	}
 }
