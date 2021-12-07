c4fd4dc28b2cbdaad2e877380d6a583c040215f5
specgen-automation <automation@specgen.io>
2021-12-07 20:33:57 +0000

    [skip ci] Generated code with specgen 2.1.991

--- models/java/test_service/models/OrderEventWrapper.java
+++ models/java/test_service/models/OrderEventWrapper.java
@@ -12,9 +12,128 @@ import com.fasterxml.jackson.annotation.JsonSubTypes.*;
 	include = JsonTypeInfo.As.WRAPPER_OBJECT
 )
 @JsonSubTypes({
-	@Type(value = OrderEventWrapperCreated.class, name = "created"),
-	@Type(value = OrderEventWrapperChanged.class, name = "changed"),
-	@Type(value = OrderEventWrapperCanceled.class, name = "canceled"),
+	@Type(value = OrderEventWrapper.Created.class, name = "created"),
+	@Type(value = OrderEventWrapper.Changed.class, name = "changed"),
+	@Type(value = OrderEventWrapper.Canceled.class, name = "canceled"),
 })
 public interface OrderEventWrapper {
+	class Created implements OrderEventWrapper {
+		@JsonUnwrapped
+		public OrderCreated data;
+
+		public Created() {
+		}
+
+		public Created(OrderCreated data) {
+			if (data == null) { throw new IllegalArgumentException("null value is not allowed"); }
+			this.data = data;
+		}
+
+		public OrderCreated getData() {
+			return data;
+		}
+
+		public void setData(OrderCreated data) {
+			if (data == null) { throw new IllegalArgumentException("null value is not allowed"); }
+			this.data = data;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (!(o instanceof Created)) return false;
+			Created that = (Created) o;
+			return Objects.equals(getData(), that.getData());
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(getData());
+		}
+
+		@Override
+		public String toString() {
+			return String.format("OrderCreated{data=%s}", data);
+		}
+	}
+
+	class Changed implements OrderEventWrapper {
+		@JsonUnwrapped
+		public OrderChanged data;
+
+		public Changed() {
+		}
+
+		public Changed(OrderChanged data) {
+			if (data == null) { throw new IllegalArgumentException("null value is not allowed"); }
+			this.data = data;
+		}
+
+		public OrderChanged getData() {
+			return data;
+		}
+
+		public void setData(OrderChanged data) {
+			if (data == null) { throw new IllegalArgumentException("null value is not allowed"); }
+			this.data = data;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (!(o instanceof Changed)) return false;
+			Changed that = (Changed) o;
+			return Objects.equals(getData(), that.getData());
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(getData());
+		}
+
+		@Override
+		public String toString() {
+			return String.format("OrderChanged{data=%s}", data);
+		}
+	}
+
+	class Canceled implements OrderEventWrapper {
+		@JsonUnwrapped
+		public OrderCanceled data;
+
+		public Canceled() {
+		}
+
+		public Canceled(OrderCanceled data) {
+			if (data == null) { throw new IllegalArgumentException("null value is not allowed"); }
+			this.data = data;
+		}
+
+		public OrderCanceled getData() {
+			return data;
+		}
+
+		public void setData(OrderCanceled data) {
+			if (data == null) { throw new IllegalArgumentException("null value is not allowed"); }
+			this.data = data;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (!(o instanceof Canceled)) return false;
+			Canceled that = (Canceled) o;
+			return Objects.equals(getData(), that.getData());
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(getData());
+		}
+
+		@Override
+		public String toString() {
+			return String.format("OrderCanceled{data=%s}", data);
+		}
+	}
 }
