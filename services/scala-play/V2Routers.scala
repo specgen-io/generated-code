85da96fc071b932f74f170d79dcf5ca75a7d5da0
specgen-automation <automation@specgen.io>
2021-12-15 23:49:40 +0000

    [skip ci] Generated code with specgen 2.1.1035

--- services/scala-play/V2Routers.scala
+++ services/scala-play/V2Routers.scala
@@ -11,7 +11,8 @@ import models.v2._
 
 class EchoRouter @Inject()(Action: DefaultActionBuilder, controller: EchoController) extends SimpleRouter {
   lazy val routeEchoBody = Route("POST", PathPattern(List(
-    StaticPart("/v2/echo/body"),
+    StaticPart("/v2"),
+    StaticPart("/echo/body"),
   )))
   def routes: Router.Routes = {
     case routeEchoBody(params@_) =>
