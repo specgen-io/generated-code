e9a2920362daeda303aa974579067f240ca5f4e5
specgen-automation <automation@specgen.io>
2022-01-13 03:31:13 +0000

    [skip ci] Generated code with specgen 2.1.1258

---
+++ clients/java-okhttp/test_client/models/Json.java
@@ -0,0 +1,14 @@
+package test_client.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jsr310.*;
+
+public class Json {
+	public static void setupObjectMapper(ObjectMapper objectMapper) {
+		objectMapper
+				.registerModule(new JavaTimeModule())
+				.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+				.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+	}
+}
