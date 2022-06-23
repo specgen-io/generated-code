19cced03526b6077946a046f86f9230a49cc57ad
specgen-automation <automation@specgen.io>
2022-06-23 06:11:31 +0000

    [skip ci] Generated code with specgen 2.1.1764

--- clients/kotlin-okhttp-jackson/test_service/clients/check/CheckClient.kt
+++ clients/kotlin-okhttp-jackson/test_service/clients/check/CheckClient.kt
@@ -1,11 +1,7 @@
 package test_service.clients.check
 
 import com.fasterxml.jackson.databind.*
-import com.fasterxml.jackson.annotation.*
-import com.fasterxml.jackson.annotation.JsonSubTypes.*
 import com.fasterxml.jackson.core.type.*
-import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.math.BigDecimal
 import java.time.*
 import java.util.*
